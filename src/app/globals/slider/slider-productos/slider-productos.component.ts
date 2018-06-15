8b0dde62594db9a5daba1d702ac97bda8220bd57
Javier Rojas <javierrojas6@gmail.com>
2018-06-14 23:45:58 -0500

    eventos

--- src/app/globals/slider/slider-productos/slider-productos.component.ts
+++ src/app/globals/slider/slider-productos/slider-productos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Producto } from '../../../models/producto';
 
 @Component({
   selector: 'app-slider-productos',
@@ -6,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slider-productos.component.scss']
 })
 export class SliderProductosComponent implements OnInit {
+  productos: Array<Producto>;
+  constructor() {
+    this.productos = new Array<Producto>();
 
-  constructor() { }
+    for (let i = 0; i < 10; i++) {
+      let p = new Producto();
+      p.nombre = `nombre ${i}`;
+      p.descripcion = `descripcion ${i}`;
+      p.imagen = `http://dfadg${i}.jpg`;
+
+      this.productos.push(p);
+    }
+  }
 
   ngOnInit() {
   }
