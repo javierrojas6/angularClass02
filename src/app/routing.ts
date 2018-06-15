d8ddcb81110402022807f26b28e531acc4f38653
Javier Rojas <javierrojas6@gmail.com>
2018-06-14 20:14:09 -0500

    pryecto con sass

--- src/app/routing.ts
+++ src/app/routing.ts
@@ -2,9 +2,11 @@ import {RouterModule, Routes} from '@angular/router'; //importar librerias de la
 
 //importar componentes que creamos para enrutarlos
 import {HomeComponent} from './pages/home/home.component'; 
+import {CategoriaComponent} from './pages/categoria/categoria.component'; 
 
 //creación de constante tipo arreglo[] con 2 objetos llamado rutas
 const routerM:Routes = [//crear condicionales 
-    {path:'', component:HomeComponent}
+    {path:'', component:HomeComponent},
+    {path:'categoria', component:CategoriaComponent}
 ];
 export const appRouter = RouterModule.forRoot(routerM);
