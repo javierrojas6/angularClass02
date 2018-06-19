a15f41afeb2866013241612c623cd070fa934ac8
Javier Rojas <javierrojas6@gmail.com>
2018-06-18 19:09:03 -0500

    funcionando

--- src/app/globals/slider/slider-productos/slider-producto-item/slider-producto-item.component.ts
+++ src/app/globals/slider/slider-productos/slider-producto-item/slider-producto-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter,Input, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 import { Producto } from '../../../../models/producto';
 
 @Component({
@@ -21,6 +21,8 @@ export class SliderProductoItemComponent implements OnInit {
     //algun proceso aca...
 
     //disparar el nuevo evento
-    this.adicionar.emit(this.producto);
+    //setTimeout(() => {
+      this.adicionar.emit(this.producto);
+    //}, 3000);
   }
 }
